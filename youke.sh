ad550dfe564ef4c99eacdeb2e6fbaefe88ef3b46
Chris Ian Fiel <ccfiel@gmail.com>
2017-03-08 15:19:13 +0800

    Initial Commit

--- youke.sh
+++ youke.sh
@@ -5,3 +5,20 @@ source "$DIR/env/bin/activate"
 git pull
 pip install -r requirements.txt
 python "$DIR/youke.py"
+PID=$!
+
+while true; do
+    sleep 30
+    git reset --hard
+    git pull
+    pip install -r requirements.txt
+
+    git fetch origin
+    reslog=$(git log HEAD..origin/master --oneline)
+    if [[ "${reslog}" != "" ]] ; then
+        git merge origin/master
+        kill -KILL $PID
+        python "$DIR/youke.py"
+        PID=$!
+	fi
+done
