7c2e32924dafdadf755c14819b5e8b4d979f73bb
Chris Ian Fiel <ccfiel@gmail.com>
2017-03-08 15:24:35 +0800

    Initial Commit

--- youke.sh
+++ youke.sh
@@ -2,17 +2,14 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 source "$DIR/env/bin/activate"
+git reset --hard
 git pull
 pip install -r requirements.txt
-python "$DIR/server.py"
+python "$DIR/server.py" &
 PID=$!
 
 while true; do
     sleep 30
-    git reset --hard
-    git pull
-    pip install -r requirements.txt
-
     git fetch origin
     reslog=$(git log HEAD..origin/master --oneline)
     if [[ "${reslog}" != "" ]] ; then
